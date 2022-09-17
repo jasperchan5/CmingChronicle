fe6126965a9b9e2e2fbf5e4d50e4e2c21d9c60b2
jasperchan5 <jscnn51011@gmail.com>
2022-09-17 20:36:11 +0800

    AA

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://cmingchronicle.herokuapp.com/",
     headers: {"Access-Control-Allow-Origin": "*"}
 })
 
