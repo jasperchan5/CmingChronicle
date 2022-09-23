a5d07964638dbc2db137fc9e396dfdbc10a29f50
jasperchan5 <jscnn51011@gmail.com>
2022-09-24 02:36:43 +0800

    backend

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-// const instance = axios.create({
-//     baseURL: "https://cmingchronicle.herokuapp.com/"
-// })
 const instance = axios.create({
-    baseURL: "http://localhost:4000/"
+    baseURL: "https://cmingchronicle.herokuapp.com/"
 })
+// const instance = axios.create({
+//     baseURL: "http://localhost:4000/"
+// })
 
 export default instance;
